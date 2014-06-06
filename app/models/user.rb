4910f6a3971cfa0be3f08f681d0f0cffcfc98326
Marc Mentis <mmentis@optonline.net>
2014-06-06 17:20:53 -0400

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
-	before_save { self.email = email.downcase}
+	before_save { self.email = email.downcase} 	# Here the block is directly passed
+
+	before_create :create_remember_token
+		# ':create_remem...' is a 'method reference', Rails looks for this method
+		# Will execute it before the controller 'create' method. I.e., before
+		# the user is saved into the database
+		# As method only used internally can make it private below
+
 
 	validates :name, presence: true, length: {maximum: 50}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -7,5 +14,18 @@ class User < ActiveRecord::Base
 					  uniqueness: {case_sensitive: false}
 
 	has_secure_password	
-	validates :password, length: {minimum: 6}			  
+	validates :password, length: {minimum: 6}
+
+	def User.new_remember_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def User.encrypt(token)
+		Digest::SHA1.hexdigest(token.to_s)
+	end
+
+	private
+		def create_remember_token
+			 self.remember_token = User.encrypt(User.new_remember_token)		  	
+		end			  
 end
